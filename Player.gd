50066db41fd9a046eee6e2899029ec9b9661629d
Chris Searle <chris@chrissearle.org>
2022-10-30 20:29:43 +0100

    End of 9 - double jump, coyote jump, jump buffer

--- Player.gd
+++ Player.gd
@@ -6,8 +6,12 @@ export(Resource) var move_data
 
 var velocity = Vector2.ZERO
 
-onready var animated_sprite = $AnimatedSprite
-onready var ladder_check = $LadderCheck
+onready var animated_sprite: = $AnimatedSprite
+onready var ladder_check: = $LadderCheck
+onready var jump_buffer_timer: = $JumpBufferTimer
+onready var coyote_jump_timer: = $CoyoteJumpTimer
+
+onready var double_jump = move_data.DOUBLE_JUMPS
 
 enum {
 	MOVE,
@@ -15,6 +19,9 @@ enum {
 }
 
 var state = MOVE
+
+var buffered_jump = false
+var coyote_jump = false
 
 func _ready():
 	randomize()
@@ -57,17 +64,23 @@ func move_state(input, delta):
 
 	animated_sprite.animation = animation
 
-	var was_jumping = !is_on_floor()
+	var was_jumping = not is_on_floor()
+	var was_on_floor = is_on_floor()
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-	var is_jumping = !is_on_floor()
-
+	var is_jumping = not is_on_floor()
 	# Handle changing frame when changing animation from jump to run
 	if (was_jumping and not is_jumping):
 		animated_sprite.animation = "Run"
 		animated_sprite.frame = 1
 
+	var just_left_ground = not is_on_floor() and was_on_floor
+	
+	if just_left_ground and velocity.y >= 0:
+		coyote_jump = true
+		coyote_jump_timer.start()
+	
 func climb_state(input, delta):
 	if not is_on_ladder():
 		state = MOVE
@@ -91,15 +104,24 @@ func is_on_ladder():
 
 func apply_jump(v, delta):
 	var amount = v.y
-
+	
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			amount = -move_data.JUMP_FORCE * delta
-	else:
-		# Close to apex
-		if Input.is_action_just_released("ui_up") and velocity.y < -(move_data.JUMP_RELEASE_FORCE * delta):
-			amount = -move_data.JUMP_RELEASE_FORCE * delta
+		reset_double_jump()
 
+	if can_jump():
+		if wants_to_jump():
+			amount = perform_jump(delta)
+	else:
+		if close_to_apex(delta):
+			amount = jump_release(delta)
+			
+		if can_double_jump():
+			amount = perform_jump(delta)
+			double_jump -= 1
+
+		if wants_to_buffer_jump():
+			perform_buffer_jump()
+		
 		# Falling - increase gravity a little
 		if velocity.y > 0:
 			amount += move_data.FAST_FALL_GRAVITY * delta
@@ -153,3 +175,39 @@ static func close_to_zero(value):
 
 static func close_to_equal(a, b, epsilon = FLOAT_EPSILON):
 	return abs(a - b) <= epsilon
+
+
+func _on_JumpBufferTimer_timeout():
+	buffered_jump = false
+
+func _on_CoyoteJumpTimer_timeout():
+	coyote_jump = false
+
+func reset_double_jump():
+	double_jump = move_data.DOUBLE_JUMPS
+
+func wants_to_jump():
+	return Input.is_action_just_pressed("ui_up") or buffered_jump
+	
+func perform_jump(delta):
+	buffered_jump = false
+	return -move_data.JUMP_FORCE * delta
+
+func perform_buffer_jump():
+	buffered_jump = true
+	jump_buffer_timer.start()
+
+func can_jump():
+	return is_on_floor() or coyote_jump
+
+func close_to_apex(delta):
+	return Input.is_action_just_released("ui_up") and velocity.y < -(move_data.JUMP_RELEASE_FORCE * delta)
+
+func jump_release(delta):
+	return -move_data.JUMP_RELEASE_FORCE * delta
+
+func can_double_jump():
+	return Input.is_action_just_pressed("ui_up") and double_jump > 0
+
+func wants_to_buffer_jump():
+	return Input.is_action_just_pressed("ui_up")
